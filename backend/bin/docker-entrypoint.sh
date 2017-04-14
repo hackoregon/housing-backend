44e65ef549548e6a935a7417c2047bb671e5970a
Troy Scott <troy_pdx@fastmail.fm>
2017-04-13 20:50:44 -0700

    removed DEPLOY_TARGET

--- backend/bin/docker-entrypoint.sh
+++ backend/bin/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo  Running docker-entrypoint.sh...
-export DEPLOY_TARGET=dev # it's always dev on your local machine
 ./bin/getconfig.sh # Necessary to pull the config file here because .dockerignore ignores the config file if it's downloaded to the container image
 
 # Disabled the migrate step to bring the container startup time down to acceptable levels
