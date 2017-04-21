5489b64d7c8e376a9061311842c103e92f91f261
Troy Scott <troy_pdx@fastmail.fm>
2017-04-20 20:25:11 -0700

    ensure CSV loader runs with local dev containers

--- backend/bin/docker-entrypoint.sh
+++ backend/bin/docker-entrypoint.sh
@@ -13,20 +13,8 @@ if [ "$DEPLOY_TARGET" == "dev" ]; then
     echo -e  docker-entrypoint.sh: Running Django migrations...
     python manage.py makemigrations --no-input
     python manage.py migrate --no-input
-fi
-
-if [ ! -d /data ]; then
-  echo "docker-entrypoint.sh: Downloading data..."
-  mkdir /data
-  wget \
-    -O /data/SoHAffordabilityDatabyNeighborhoodUpload.csv \
-    https://raw.githubusercontent.com/hackoregon/housing-backend/datasources/SoHAffordabilityDatabyNeighborhoodUpload.csv
-  echo "docker-entrypoint.sh: Migrating database..."
-  while ! ./manage.py migrate >> /dev/null 2>&1 ; do
-    sleep 1
-  done
-  echo "docker-entrypoint.sh: Loading data..."
-  ./manage.py shell --command="import housing_backend.loader"
+    echo -e docker-entrypoint.sh: Download raw CSV and load database...
+    ./start-dev-server.sh
 fi
 
 python manage.py collectstatic --no-input
@@ -34,5 +22,4 @@ python manage.py collectstatic --no-input
 echo -e  docker-entrypoint.sh: Running gunicorn...
 # Fire up a lightweight frontend to host the Django endpoints - gunicorn was the default choice
 # gevent used to address ELB/gunicorn issue here https://github.com/benoitc/gunicorn/issues/1194
-#gunicorn backend.wsgi:application -b :8000 --worker-class 'gevent' --workers 3 --keep-alive 60
 gunicorn backend.wsgi:application -b :8000 --worker-class 'gevent' --workers 1
