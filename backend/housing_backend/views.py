1675ad0a83de6234480aa42e2b48bc06370d8f45
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-04-23 16:41:03 -0700

    add views for tooltip endpoints

--- backend/housing_backend/views.py
+++ backend/housing_backend/views.py
@@ -1,8 +1,9 @@
 from django_filters.rest_framework import FilterSet
 from django_filters import NumberFilter, CharFilter
 
-from housing_backend.models import Affordable, NeighborhoodRent, HousingProductionVsCost
-from housing_backend.serializers import AffordableSerializer, RentSerializer, ProdVsCostSerializer
+from housing_backend.models import Affordable, NeighborhoodRent, HousingProductionVsCost, HHToolTip, PopToolTip
+from housing_backend.serializers import AffordableSerializer, RentSerializer, ProdVsCostSerializer,\
+                                        HHToolTipSerializer, PopToolTipSerializer
 
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
@@ -52,3 +53,23 @@ class RentDetail(RetrieveAPIView):
 class ProdVsCostList(ListAPIView):
     queryset = HousingProductionVsCost.objects.all()
     serializer_class = ProdVsCostSerializer
+
+class RentFilter(FilterSet):
+    rent_amt = NumberFilter(name="rent_amt", lookup_expr="rent")
+    rent_amt_gt = NumberFilter(name="rent_amt", lookup_expr="gt")
+    rent_amt_lt = NumberFilter(name="rent_amt", lookup_expr="lt")
+    housing_size = CharFilter(name="housing_size__household_type")
+
+    class Meta:
+        model = NeighborhoodRent
+        fields = ['rent_amt', 'housing_size']
+
+
+class HHToolTip(ListAPIView):
+    queryset = HHToolTip.objects.all()
+    serializer_class = HHToolTipSerializer
+
+
+class PopToolTip(ListAPIView):
+    queryset = PopToolTip.objects.all()
+    serializer_class = PopToolTipSerializer
