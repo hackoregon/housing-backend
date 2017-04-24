3ee077fa0b56c83a05e3f701defdba141dace19f
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-04-23 17:05:22 -0700

    add makemigratoins and migrate cmds to deploy scripts

--- backend/bin/docker-entrypoint.sh
+++ backend/bin/docker-entrypoint.sh
@@ -5,8 +5,10 @@ echo  Running docker-entrypoint.sh...
 # Necessary to pull the config file here because .dockerignore ignores the config file if it's downloaded to the container image
 ./bin/getconfig.sh
 
-# Disabled the migrate step to bring the container startup time down to acceptable levels
-#python manage.py migrate --no-input
+# Re-enabled the migrations stuff temporarily after model changes - @jaymcgrath
+# Disable after my 4/23/17 PR is deployed
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
 # Fire up a lightweight frontend to host the Django endpoints - gunicorn was the default choice
